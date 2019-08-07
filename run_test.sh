ac9cfc565151a50f595526e7e6b23919e1c51351
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-08-08 01:26:09 +0800

    fix a cmd error

--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,4 @@
 # ./run_test.sh splitID dataPointFileName
 
 # For OWASP
-python3 ggnn_sparse.py --data_dir data/embed/APIReplacement/DateGetTime --restore logs/train_$1_undertest.pickle --valid_file $2 --train_file $2
+python3 ggnn_sparse.py --predict --data_dir data/embed/APIReplacement/DateGetTime --restore logs/train_$1_undertest.pickle --valid_file $2 --train_file $2
