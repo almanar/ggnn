8b8a5d7d5777765d35df55d505530d749f5a1758
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-08-15 02:37:32 +0800

    add argument check

--- run_exp.sh
+++ run_exp.sh
@@ -2,4 +2,9 @@
 
 # ./run_exp.sh APIReplacement Mkdir 0
 
+if [ ! $# == 3 ]; then 
+	echo "Usage: $0 APIReplacement Mkdir 0"
+	exit
+fi
+
 python3 ggnn_sparse.py --data_dir data/embed/$1/$2 --valid_file valid_$1_$2_$3.json --train_file train_$1_$2_$3.json
--- run_test.sh
+++ run_test.sh
@@ -4,5 +4,9 @@
 # Usage
 # ./run_test.sh APIReplacement Mkdir
 
-# For OWASP
+if [ ! $# == 2 ]; then 
+	echo "Usage: $0 APIReplacement Mkdir"
+	exit
+fi
+
 python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json
