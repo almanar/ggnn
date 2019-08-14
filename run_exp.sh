62bb757e2b32c25279ad45eac21116fb9af9fe63
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-08-15 01:34:49 +0800

    add demo

--- run_exp.sh
+++ run_exp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# ./run_exp.sh APIReplacement Mkdir 0
+
 python3 ggnn_sparse.py --data_dir data/embed/$1/$2 --valid_file valid_$1_$2_$3.json --train_file train_$1_$2_$3.json
--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 
 
 # Usage
-# ./run_test.sh splitID dataPointFileName
+# ./run_test.sh APIReplacement Mkdir
 
 # For OWASP
 python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json
