738d9427c60572c646df3e06b16c6398d3e55f16
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-08-15 01:33:01 +0800

    update command

--- run_exp.sh
+++ run_exp.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-python3 ggnn_sparse.py --data_dir data/embed/APIReplacement/DateGetTime --valid_file valid_APIReplacement_DateGetTime_0.json --train_file train_APIReplacement_DateGetTime_0.json
-python3 ggnn_sparse.py --data_dir data/embed/APIReplacement/DateGetTime --valid_file valid_APIReplacement_DateGetTime_1.json --train_file train_APIReplacement_DateGetTime_1.json
-python3 ggnn_sparse.py --data_dir data/embed/APIReplacement/DateGetTime --valid_file valid_APIReplacement_DateGetTime_2.json --train_file train_APIReplacement_DateGetTime_2.json
-python3 ggnn_sparse.py --data_dir data/embed/APIReplacement/DateGetTime --valid_file valid_APIReplacement_DateGetTime_3.json --train_file train_APIReplacement_DateGetTime_3.json
-python3 ggnn_sparse.py --data_dir data/embed/APIReplacement/DateGetTime --valid_file valid_APIReplacement_DateGetTime_4.json --train_file train_APIReplacement_DateGetTime_4.json
+python3 ggnn_sparse.py --data_dir data/embed/$1/$2 --valid_file valid_$1_$2_$3.json --train_file train_$1_$2_$3.json
