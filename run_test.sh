c4fa9b09b91458f7fe33dfaa32299e1e6cf91056
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-08-17 21:30:32 +0800

    can set epoch in script

--- run_test.sh
+++ run_test.sh
@@ -4,9 +4,15 @@
 # Usage
 # ./run_test.sh APIReplacement Mkdir
 
-if [ $# != 2 ]; then 
+if [ $# < 2 ]; then 
 	echo "Usage: $0 APIReplacement Mkdir"
 	exit
 fi
 
-python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json
+if [ $# == 2 ]; then 
+	python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json
+fi
+
+if [ $# == 3 ]; then 
+	python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json --epoch $3
+fi
