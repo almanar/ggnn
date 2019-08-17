199c2d742aef791da6184d5a12e2bd5deaed0429
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-08-17 21:35:59 +0800

    update script

--- run_test.sh
+++ run_test.sh
@@ -4,15 +4,15 @@
 # Usage
 # ./run_test.sh APIReplacement Mkdir
 
-if [ $# < 2 ]; then 
+if [ $# -lt 2 ]; then 
 	echo "Usage: $0 APIReplacement Mkdir"
 	exit
 fi
 
-if [ $# == 2 ]; then 
+if [ $# -eq 2 ]; then 
 	python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json
 fi
 
-if [ $# == 3 ]; then 
+if [ $# -eq 3 ]; then 
 	python3 ggnn_sparse.py --predict --data_dir data/embed/$1/$2 --valid_file test_$1_$2.json --train_file train_$1_$2.json --epoch $3
 fi
